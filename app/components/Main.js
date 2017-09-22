ed0ff5b4868fe5ad8baa5de335e42954812afb26
Andy <andyepatrick@gmail.com>
2017-09-22 14:47:11 -0400

    structure setup

---
+++ app/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Main extends React.Component{
+	constructor(){
+		super()
+	}
+	render(){
+		return(
+			<div className="container">
+			<h1>Hello World</h1>
+			</div>
+			)
+	}
+};
+
+export default Main;
--- app/config/Router.js
+++ app/config/Router.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import Main from '../components/Main';
+
+const Router = () => (
+	<div id="app-container">
+      <Main />
+  </div>
+)
+
+export default Router;
